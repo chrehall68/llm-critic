cc46ee76d4b76777007f9b573a94e1f456cffbd7
chrehall68 <chrehall68@gmail.com>
2024-05-24 11:27:13 -0700

    add dev set to baseline for more robust baseline

--- llm_critic/core/baseline.py
+++ llm_critic/core/baseline.py
@@ -23,12 +23,19 @@ def run_baseline(
     model, tokenizer = setup_model(model_name)
     splits = ds.train_test_split(test_size=0.2, train_size=0.8)
     train = splits["train"]
-    test = splits["test"]
+    test = splits["test"]  # 20% of original dataset
+    splits = train.train_test_split(test_size=0.125, train_size=0.875)
+    train = splits["train"]  # 70% of original dataset
+    dev = splits["test"]  # 10% of original dataset
+    metrics = evaluate.combine(["accuracy", "recall", "precision", "f1"])
 
     train = train.map(lambda e: tokenizer(e["abstract"], truncation=True)).map(
         lambda e: {"label": 1 if e["accepted"] else 0}
     )
     test = test.map(lambda e: tokenizer(e["abstract"], truncation=True)).map(
+        lambda e: {"label": 1 if e["accepted"] else 0}
+    )
+    dev = dev.map(lambda e: tokenizer(e["abstract"], truncation=True)).map(
         lambda e: {"label": 1 if e["accepted"] else 0}
     )
 
@@ -37,18 +44,25 @@ def run_baseline(
         output_dir=output_dir,
         overwrite_output_dir=True,
         num_train_epochs=3,
+        logging_steps=50,
+        evaluation_strategy="epoch",
     )
     trainer = Trainer(
         model,
         args,
         data_collator=DataCollatorWithPadding(tokenizer),
         train_dataset=train,
+        eval_dataset=dev,
         tokenizer=tokenizer,
+        compute_metrics=lambda model_preds: metrics.compute(
+            predictions=model_preds.predictions.argmax(axis=-1),
+            references=model_preds.label_ids,
+        ),
     )
     trainer.train()
 
     outputs = trainer.predict(test)
     metrics = evaluate.combine(["accuracy", "recall", "precision", "f1"])
-    results = metrics.compute(outputs, test["label"])
+    results = metrics.compute(outputs.predictions.argmax(axis=-1), test["label"])
 
     pk.dump(results, open(f"{model_name}_baseline_results.pk", "wb"))
