9c39a895df12ff794b1e190c99e3e74e17fe94d2
chrehall68 <chrehall68@gmail.com>
2025-07-09 22:57:54 -0700

    edit setup to include requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="llm_critic", packages=setuptools.find_packages(exclude=["scripts"])
+    name="llm_critic",
+    packages=setuptools.find_packages(exclude=["scripts"]),
+    requires=open("requirements.txt").read().split("\n"),
 )
